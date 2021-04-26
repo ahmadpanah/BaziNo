8fc53d305fac1592520c6fd935e6d356f994a0a4
Hossein <djalicrt@gmail.com>
2021-04-26 19:12:32 +0430

    impl MVVM Architecture Category Model - Bind - Adapter

---
+++ app/src/main/java/ir/shariaty/bazino/CategoryModel.java
@@ -0,0 +1,39 @@
+package ir.shariaty.bazino;
+
+public class CategoryModel {
+    private String categoryId, categoryName, categoryImage;
+
+    public CategoryModel(String categoryId, String categoryName, String categoryImage) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.categoryImage = categoryImage;
+    }
+
+    public CategoryModel() {}
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryImage() {
+        return categoryImage;
+    }
+
+    public void setCategoryImage(String categoryImage) {
+        this.categoryImage = categoryImage;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+
+}
